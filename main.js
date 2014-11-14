d27d0cc4c69bbe3767514c5b054841f89112da61
Toni Strandell <toni.strandell@reaktor.fi>
2014-11-15 00:50:16 +0200

    Resize and loop video

--- main.js
+++ main.js
@@ -6,3 +6,29 @@
      |___/ \___|\__,_|_|    \_____/\__,_|\___|\__, |
                                                __/ |
                                               |___/   */
+
+$(document).ready(function() {
+
+  function adjustVideoSize() {
+    var maxVideoWidth = 1440
+    var aspectRatio = 16 / 9
+    var windowWidth = $(window).width()
+    var videoWidth = Math.min(maxVideoWidth, windowWidth)
+    var videoHeight = videoWidth / aspectRatio
+
+    $('.introduction').css('height', videoHeight)
+    $('.video').add('.video-container').css({
+      width: videoWidth + 'px',
+      height: videoHeight + 'px'
+    })
+  }
+
+  var noVideoAutoPlay = /(iPad|iPhone|iPod|Windows Phone|Android)/g.test(navigator.userAgent)
+
+  if (noVideoAutoPlay) {
+    $('body').addClass('no-video-autoplay')
+    $('.video').remove()
+  }
+  $(window).on('resize', adjustVideoSize)
+  adjustVideoSize()
+})
